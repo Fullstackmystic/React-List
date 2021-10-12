6b880378075ba938720a600eda4cf95e07cf46ba
Prateek Sogra <80418551+Fullstackmystic@users.noreply.github.com>
2021-10-13 00:49:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import List from './Components/Rater.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React List</h1>
+      </header>
+      <body>
+        <List/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
